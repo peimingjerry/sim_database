cc753b228751f69b239d3cbe8a72209721e2ae65
peimingjerry <zhoupeiming@qq.com>
2016-03-06 01:16:06 +0800

    Create util.py

---
+++ util.py
@@ -0,0 +1,9 @@
+
+############################
+def getDumpOffset(level):
+    offset = ''
+    for i in range(level):
+        offset += '    '
+    return offset
+
+printDebug = True #for debug print
